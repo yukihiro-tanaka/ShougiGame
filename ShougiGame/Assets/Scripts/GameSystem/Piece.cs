ead5e1c63f7d53a1ea3f00abda017637b20ef471
田中幸浩 <>
2020-09-27 20:21:46 +0900

    ・移動中に歩きモーションに切り替わるように修正 ・移動前に進行方向に向くように修正

--- ShougiGame/Assets/Scripts/GameSystem/Piece.cs
+++ ShougiGame/Assets/Scripts/GameSystem/Piece.cs
@@ -11,18 +11,25 @@ public class Piece : MonoBehaviour
     public bool m_isAblePromote { get; private set; }
     private GameObject m_aura = default;
     private Vector3 m_targetTransformPosition;
+    Quaternion m_targetTransformRotation;
     private Animator m_pieceAnimator;
 
 
     private void Start() {
         m_pieceAnimator = GetComponent<Animator>();
     }
+
     private void Update() {
-        if (transform.position == m_targetTransformPosition) {
-            m_pieceAnimator.SetBool("isWalking", false);
-        } else {
-            transform.position = Vector3.MoveTowards(transform.position, m_targetTransformPosition, 2 * Time.deltaTime);
+        if (!isReachTargetTransformRotation()) {
+            transform.rotation = Quaternion.Slerp(transform.rotation, m_targetTransformRotation, Time.deltaTime);
+            return;
+        }
+        if (!isReachTargetTransformPosition()) {
             m_pieceAnimator.SetBool("isWalking", true);
+            transform.position = Vector3.MoveTowards(transform.position, m_targetTransformPosition, 2 * Time.deltaTime);
+        } else {
+            m_pieceAnimator.SetBool("isWalking", false);
+            m_targetTransformRotation.y = ((m_whose == Who.One) ? 0.0f : 180.0f);
         }
     }
 
@@ -43,6 +50,7 @@ public class Piece : MonoBehaviour
             transform.Rotate(new Vector3(0, 180, 0));
         }
         m_targetTransformPosition = transform.position;
+        m_targetTransformRotation = transform.rotation;
     }
 
     public void promote()
@@ -57,11 +65,30 @@ public class Piece : MonoBehaviour
     {
         m_position = position;
         m_targetTransformPosition = targetTransformPosition;
+        float diffX = transform.position.x - targetTransformPosition.x;
+        float diffZ = transform.position.z - targetTransformPosition.z;
+        float rotation = Mathf.Atan(diffX / diffZ) * Mathf.Rad2Deg + ((m_whose == Who.One) ? 0.0f : 180.0f);
+        m_targetTransformRotation = Quaternion.Euler(0, rotation, 0);
     }
 
-
-    private void OnChangedIsPromoted()
+    private bool isReachTargetTransformRotation()
     {
-        Debug.Log( "log" );
+        float animetionRangeY = 2.0f;
+        if (Mathf.Abs(Mathf.DeltaAngle(m_targetTransformRotation.eulerAngles.y, transform.rotation.eulerAngles.y)) > animetionRangeY) {
+            return false;
+        }
+        return true;
+    }
+    private bool isReachTargetTransformPosition()
+    {
+        float animetionRangeX = 0.05f;
+        float animetionRangeZ = 0.05f;
+        if (Mathf.Abs(m_targetTransformPosition.x - transform.position.x) > animetionRangeX) {
+            return false;
+        }
+        if (Mathf.Abs(m_targetTransformPosition.z - transform.position.z) > animetionRangeZ) {
+            return false;
+        }
+        return true;
     }
 }
